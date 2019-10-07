5b806ec27faff6c181f17f92b79cf260d0189bb7
ihatemodels <gtechno@protonmail.com>
2019-10-07 22:09:34 +0300

    IP log added

--- rdp-telegram.ps1
+++ rdp-telegram.ps1
@@ -1,11 +1,12 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $event = Get-WinEvent -FilterHashtable @{Logname='Security';Id=4624} -MaxEvents 1
 $message= ""
+$ip = $event.Properties[18].Value
 $domain = $event.Properties[2].Value
 $user = $event.Properties[5].Value
 $datetime = $event.TimeCreated.ToString("dd.MMM.yyyy HH:mm:ss")
 
-$message = "*RDP LOGIN* `nSystem: $domain\$user `nDate: $datetime"
+$message = "*RDP LOGIN* `nSystem: $domain\$user `nDate: $datetime `n IPAddres: $ip"
 
 $botToken = 'ADD UR TOKEN HERE'
 $chatID = 'ADD UR CHAT ID HERE'
